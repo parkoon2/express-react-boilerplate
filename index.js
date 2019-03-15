ee25f23caed82cb23fcfc399f03d1b63529cfac7
parkoon <par_koon@naver.com>
2019-03-15 14:22:35 +0900

    기본 server index 코딩

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const APP_DIR = path.join(__dirname, 'client/dist')
+
+app.use(express.static(APP_DIR))
+
+app.get('/api/v1/list', (req, res) => {
+    let list = ['apple', 'banana', 'melon']
+    res.json(list)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log('App is listening on port' + PORT)
+})
