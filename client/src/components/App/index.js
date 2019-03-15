c0e81e95a0e0c788011b16889efab9f548c783b0
parkoon <par_koon@naver.com>
2019-03-15 14:33:27 +0900

    api 확인

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.getList()
+  }
+
+  getList = () => {
+    fetch('/api/v1/list')
+      .then(res => res.json())
+      .then(list => console.log(list))
+  }
+
   render() {
     return (
       <div>
